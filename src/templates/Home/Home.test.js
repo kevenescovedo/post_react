a98bb7b8e21fd28c41186fb92f0da2ab3fb3f6fc
DESKTOP-Q1VIMM1\keven <kevenescovedo1916@gmail.com>
2022-10-04 17:04:15 -0300

    I'm displaying the posts of the people the user follows

--- src/templates/Home/Home.test.js
+++ src/templates/Home/Home.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from '.';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("<Home/>", () =>{
+  it('test one', () => {
+    expect(1).toBe(1);
+   });
+   it('test two', () => {
+    expect(1).toBe(1);
+   });
+   it('test three', () => {
+    expect(1).toBe(1);
+   });
+   
+})
